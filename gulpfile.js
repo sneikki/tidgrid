9c3df71001da6b7cfa83413ae0ef38512e3e6515
Nuutti Varvikko <nvarvikko@gmail.com>
2020-09-23 17:48:55 +0300

    create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp')
+var sass = require('gulp-dart-sass')
+var rename = require('gulp-rename')
+var cleancss = require('gulp-clean-css')
+var sourecmaps = require('gulp-clean-css')
+var autoprefixer = require('gulp-autoprefixer')
+var del = require('del')
+
+gulp.task('transpile', function transpile() {
+    return gulp
+        .src('scss/tidgrid.scss')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(sass({ includePaths: ['node_modules'] }).on('error', sass.logError))
+        .pipe(rename('tidgrid.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('minify', function() {
+    return gulp
+        .src('./dist/tidgrid.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleancss({ level: 2 }))
+        .pipe(rename('tidgrid.min.csss'))
+        .pipe(sourecmaps.write('.'))
+        .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('build', gulp.series('transpile', 'minify'))
+
+gulp.task('transpile:watch', function() {
+    gulp.watch('./sass/**/*.sass', gulp.task('transpile'))
+})
+
+gulp.task('build:watch', function() {
+    gulp.watch('./sass/**/*.sass', gulp.task('build'))
+})
+
+gulp.task('clean', async function() {
+    return await del('dist')
+})
